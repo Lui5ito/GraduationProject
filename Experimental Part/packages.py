316bb0cb883a239f08d914167b7f80cc24ab215d
Lui5ito <louis.allain@gmail.com>
2024-03-10 22:19:37 +0100

    adding joblib

--- Experimental Part/packages.py
+++ Experimental Part/packages.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import time
+import os
 
 # Specifically for the Rotor37 Dataset importation and visualization
 import h5py
@@ -11,12 +13,13 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # For saving metadata
 import json
+import joblib
 
 # For optimized subsampling
 from scipy.spatial.distance import cdist
 
 # General purpose machine learning
-from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score
 from sklearn.model_selection import GridSearchCV, KFold, ParameterGrid, train_test_split
 from sklearn.gaussian_process import kernels
 from sklearn.preprocessing import StandardScaler
